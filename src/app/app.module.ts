94012e4422ace8fa8ecea4bf4680104db66f5323
Patrick <pdn6991@gmail.com>
2022-02-21 08:10:48 -0500

    clean up app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './board/square/square.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+import { KeyComponent } from './keyboard/key/key.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SquareComponent,
+    KeyboardComponent,
+    KeyComponent,
+    TitleBarComponent
   ],
   imports: [
     BrowserModule
